65e8fe5eb195ff7f077cbccd577fdc85cfb11436
Herbert Baier <baier@itbaier.de>
2024-06-13 17:06:44 +0200

    project/launched

---
+++ src/test/java/de/uniwuerzburg/zpd/ocr4all/application/calamari/msa/Ocr4allAppCalamariMsaApplicationTests.java
@@ -0,0 +1,33 @@
+/**
+ * File:     Ocr4allAppCalamariMsaApplicationTests.java
+ * Package:  de.uniwuerzburg.zpd.ocr4all.application.ocrd.msa
+ * 
+ * Author:   Herbert Baier (herbert.baier@uni-wuerzburg.de)
+ * Date:     13.06.2024
+ */
+package de.uniwuerzburg.zpd.ocr4all.application.calamari.msa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Define tests for Calamari server application.
+ *
+ * @author <a href="mailto:herbert.baier@uni-wuerzburg.de">Herbert Baier</a>
+ * @version 1.0
+ * @since 17
+ */
+@SpringBootTest
+class Ocr4allAppCalamariMsaApplicationTests {
+
+	/**
+	 * Performs a critical check to ensure that the Spring application context can
+	 * be loaded successfully.
+	 * 
+	 * @since 17
+	 */
+	@Test
+	void contextLoads() {
+	}
+
+}
